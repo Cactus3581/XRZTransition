b5f985e5a9ceffbd4393db3a57eda5c872c7f81a
xiaruzhen <tytyty3581@163.com>
2017-04-02 17:17:43 +0800

    add

--- XRZTransition/XRZTransition/ViewController.m
+++ XRZTransition/XRZTransition/ViewController.m
@@ -16,16 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-<<<<<<< HEAD
+
+
     
+
     NSLog(@"xrz");
+    NSLog(@"xrz1");
 
-    
-    
-=======
-    NSLog(@"otherTest");
 
->>>>>>> 406b16be68a3b1a6c3c6381864fc9c266b6e4ff8
 
     // Do any additional setup after loading the view, typically from a nib.
 }
